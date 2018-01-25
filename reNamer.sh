adb96c6db9c50de363350b497335fcc184081ec6
huruji <594613537@qq.com>
2018-01-25 18:20:57 +0800

    添加命名模式与过滤功能

--- reNamer.sh
+++ reNamer.sh
@@ -1,23 +1,66 @@
 #!/bin/bash -v
 
-if [[ $1 ]]; then
-	document=$1
-else
-	document=`pwd`
+
+folder=$1
+patternBefroe=''
+patternAfter=''
+files=()
+
+function reName(){
+	i=1
+	for file in files
+	do
+		ext=${file##*.}
+		oriName="${folder}/${file}"
+		if [[ $file != $ext ]]; then
+			name="${folder}/${patternBefroe}${i}${patternAfter}.$ext"
+		else
+			name="${folder}/${patternBefroe}${i}${patternAfter}"
+		fi
+		`mv $oriName $name`
+		((i++))
+	done
+
+	echo "done"
+}
+
+if [[ ${docu:0:1} = "-" ]]; then
+	folder=`pwd`
 fi
 
-i=1
-for files in `ls $1`
-do
-	ext=${files##*.}
-	oriName="${document}/${files}"
-	if [[ $files != $ext ]]; then
-		name="${document}/${i}.$ext"
-	else
-		name="${document}/${i}"
-	fi
-	`mv $oriName $name`
-	((i++))
-done
 
-echo "done"
+
+if [[ ! -d $folder ]]; then
+	echo "$folder is not exists!"
+else
+	while getopts "p:e:a" arg
+	do
+		case $arg in
+			a)
+			files=`ls $folder -l`
+			;;
+			e)
+				files=`ls $folder -l | grep '.$OPTARG'`
+			;;
+			p)
+				patternBefroe=${OPTARG%%/.*}
+				patternAfter=${OPTARG##*/.}
+			;;
+		esac
+	done
+	echo ${files[*]}
+	echo "patternAfter=$patternAfter"
+	echo "patternBefroe=$patternBefroe"
+	echo "floder=$folder"
+	reName
+
+fi
+
+
+
+# if [[ $1 ]]; then
+# 	document=$1
+# else
+# 	document=`pwd`
+# fi
+
